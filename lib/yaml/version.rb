170fa712cf92b7a114937e371643f366441afd60
Hiroshi SHIBATA <hsbt@ruby-lang.org>
2019-08-06 12:43:06 +0900

    bundle gem yaml

---
+++ lib/yaml/version.rb
@@ -0,0 +1,3 @@
+module Yaml
+  VERSION = "0.1.0"
+end
---
+++ test/yaml_test.rb
@@ -0,0 +1,11 @@
+require "test_helper"
+
+class YamlTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Yaml::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
